35f3bb3a08b956f28a1c79dcdfb96147cf090348
Sohan <sohan.j12@gmail.com>
2023-10-18 16:00:36 +0530

    useEffect moved from NavBAr to InitUser component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -40,92 +40,6 @@ export default function Navbar() {
   const router = useRouter();
   const setUserState = useSetRecoilState(userState);
 
-  useEffect(() => {
-    magic.user.isLoggedIn().then(async (magicIsLoggedIn: boolean) => {
-      setIsLoggedIn(magicIsLoggedIn);
-      if (magicIsLoggedIn) {
-        metadata = await magic.user.getMetadata();
-        // setPublicAddress(metadata.publicAddress || "");
-        // setUserMetadata(metadata);
-
-        const idToken = await magic.user.getIdToken();
-
-        // Format the cookie string
-        const cookieValue = `token=${idToken}; path=/`;
-        // Set the cookie
-        document.cookie = cookieValue;
-        const createUserResponse = await axios({
-          method: "POST",
-          url: "/api/user/createUser",
-          data: {
-            id: metadata.issuer,
-            email: metadata.email,
-            publicAddress: metadata.publicAddress,
-          },
-        });
-        if (createUserResponse) {
-          const getFollows = await axios({
-            method: "GET",
-            url: "/api/getFollows",
-            params: {
-              userId: createUserResponse.data.user.id,
-            },
-          });
-          if (getFollows) {
-            setUserState({
-              isLoading: false,
-              email: createUserResponse.data.user.email,
-              role: "user",
-              id: createUserResponse.data.user.id,
-              username: createUserResponse.data.user.username,
-              isVerified: createUserResponse.data.user.isVerified,
-              isActivated: createUserResponse.data.user.isActivated,
-              likes: createUserResponse.data.user.likes,
-              followers: getFollows.data.followers,
-              followees: getFollows.data.following,
-              publicAddress: createUserResponse.data.user.publicAddress,
-              balance: null,
-            });
-          } else {
-            setUserState({
-              isLoading: false,
-              email: createUserResponse.data.user.email,
-              role: "user",
-              id: createUserResponse.data.user.id,
-              username: createUserResponse.data.user.username,
-              isVerified: createUserResponse.data.user.isVerified,
-              isActivated: createUserResponse.data.user.isActivated,
-              likes: createUserResponse.data.user.likes,
-              followers: null,
-              followees: null,
-              publicAddress: createUserResponse.data.user.publicAddress,
-              balance: null,
-            });
-          }
-          setIsLoggedIn(true);
-        }
-
-        try {
-          const publicKey = new web3.PublicKey(metadata.publicAddress || "");
-          const solana = new web3.Connection(RPC_URL);
-          const balance = await solana.getBalance(publicKey);
-
-          setUserState((prevUserState) => ({
-            ...prevUserState,
-            balance: balance,
-          }));
-
-          console.log("Nav-Bar - Balance set to...", balance);
-        } catch (error) {
-          // Handle the error, e.g., user not found
-          console.log(error);
-        }
-
-        console.log("metadata from useEffect in navbar....", metadata); //remove later
-      }
-    });
-  }, []);
-
   const handleLogin = async () => {
     console.log("handling log in....");
 
@@ -255,7 +169,7 @@ export default function Navbar() {
     setIsModalOpen(false);
   };
 
-  return isLoggedIn ? (
+  return user.id ? (
     <div>
       <div className="sm:flex justify-between p-5">
         <div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 
 import LandingPage from "@/components/LandingPage";
+import InitUser from "@/components/InitUser";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
+      <InitUser />
       <LandingPage />
       <div className="min-h-[50vh]"></div>
     </div>
